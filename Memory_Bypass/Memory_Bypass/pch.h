67b06ea6f98c0dfd02dcda9dabc45aee48febb6b
邢传真 <2895371049@qq.com>
2022-01-20 14:05:22 +0800

    添加项目文件。

---
+++ Memory_Bypass/Memory_Bypass/pch.h
@@ -0,0 +1,16 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#ifndef PCH_H
+#define PCH_H
+
+// 添加要在此处预编译的标头
+#include "framework.h"
+extern "C" _declspec(dllexport) HANDLE GetProcessHandle(int nID);
+extern "C" _declspec(dllexport) DWORD GetProcId(const char* procName);
+extern "C" _declspec(dllexport) uintptr_t GetModuleBaseAddress(DWORD procId, const char* modName);
+extern "C" _declspec(dllexport) bool WriteMemory(HANDLE Handle, long long Address, char Buffer[1]);
+#endif //PCH_H
---
+++ Minecraft_Memory_Bypass/Operate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft_Memory_Bypass
+{
+    class Operate
+    {
+        /// <summary>
+        /// 执行cmd命令，返回执行结果
+        /// </summary>
+        /// <param name="command">命令内容</param>
+        /// <returns>返回执行结果字符串</returns>
+        public static string RunCmd(string command)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";         //确定程序名
+            p.StartInfo.Arguments = "/c " + command;   //确定程式命令行
+            p.StartInfo.UseShellExecute = false;      //Shell的使用
+            p.StartInfo.RedirectStandardInput = true;  //重定向输入
+            p.StartInfo.RedirectStandardOutput = true; //重定向输出
+            p.StartInfo.RedirectStandardError = true;  //重定向输出错误
+            p.StartInfo.CreateNoWindow = true;        //设置置不显示示窗口
+            p.Start();
+            return p.StandardOutput.ReadToEnd();      //输出出流取得命令行结果果
+        }
+        /// <summary>
+        /// 截取字符串
+        /// </summary>
+        /// <param name="sourse">目标</param>
+        /// <param name="startstr">从这里</param>
+        /// <param name="endstr">到这里</param>
+        /// <returns>返回不包含"从这里","到这里"的字符串</returns>
+        public static string Substring(string sourse, string startstr, string endstr)
+        {
+            string result = string.Empty;
+            int startindex, endindex;
+            try
+            {
+                startindex = sourse.IndexOf(startstr);
+                if (startindex == -1)
+                {
+                    return result;
+                }
+                string tmpstr = sourse.Substring(startindex + startstr.Length);
+                endindex = tmpstr.IndexOf(endstr);
+                if (endindex == -1)
+                {
+                    return result;
+                }
+                result = tmpstr.Remove(endindex);
+            }
+            catch { }
+            return result;
+        }
+    }
+}
